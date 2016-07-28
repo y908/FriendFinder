47fc883a939e91f0be5bdb578297dabd96c968dc
Yana Yakubchik <Yana Yakubchik>
2016-07-27 21:35:05 -0400

    files are connected

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends.
+// I just set it equal to a "dummy" friend. 
+// 
+// ===============================================================================
+
+var friendArray = [
+  {
+  "name":"Lady Mormont of Bear Island",
+  "photo":"http://i.imgur.com/RI27xcF.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+ }
+];
+
+
+// export the array. 
+module.exports = friendArray;
