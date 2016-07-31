1900cc31e40faa284aa12465f24b6e8ab4eae7dc
Yana Yakubchik <Yana Yakubchik>
2016-07-31 19:51:14 -0400

    IT WORKSgit add -A made changes to api route to compare

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,59 +1,58 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the friends.
-// I just set it equal to a "dummy" friend. 
-// 
-// ===============================================================================
-
 var friendArray = [
   {
   "name":"Lady Mormont of Bear Island",
   "photo":"http://i.imgur.com/RI27xcF.jpg",
   "scores":[
      5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
      1
    ]
- },
- {
-  "name":"Lady Mormont of Bear Island",
-  "photo":"http://i.imgur.com/RI27xcF.jpg",
+ },  {
+  "name":"Queen Kaleesi",
+  "photo":"http://images6.fanpop.com/image/photos/37200000/Daenerys-Targaryen-Season-4-daenerys-targaryen-37215658-2832-4256.jpg",
   "scores":[
-     5,
      1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
+     5
    ]
- },
- {
-  "name":"Lady Mormont of Bear Island",
-  "photo":"http://i.imgur.com/RI27xcF.jpg",
+ },  {
+  "name":"Sir Jorah",
+  "photo":"http://www.ew.com/sites/default/files/i/2015/05/10/got-iain-glen.jpg",
   "scores":[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
+     3,
+     3
    ]
  }
+/* {
+  "name":"Lady Mormont of Bear Island",
+  "photo":"http://i.imgur.com/RI27xcF.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+ },
+ {
+  "name":"Lady Mormont of Bear Island",
+  "photo":"http://i.imgur.com/RI27xcF.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+ }*/
 ];
 
 
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,64 +1,61 @@
-// ===============================================================================
-// LOAD DATA
+var friendsData     = require('../data/friends.js');
 
-// ===============================================================================
-
-var friendData = require('../data/friends.js');
 var path      = require('path');
 
-// ===============================================================================
+// ============================
 // ROUTING
-// ===============================================================================
+// ============================
 
 module.exports = function(app){
 
-  // API GET Requests
-  // Below code handles when users "visit" a page. 
-  // In each of the below cases when a user visits a link 
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table) 
-  // ---------------------------------------------------------------------------
-
   app.get('/api/friends', function(req, res){
-    res.json(friendData);
+    res.json(friendsData);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate Javascript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the friendData array)
-  // ---------------------------------------------------------------------------
 
-  app.post('/api/friend', function(req, res){
+  app.post('/api/friends', function(req, res){
+  
 
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table 
-  /*  if(friendData.length < 5 ){
-      friendData.push(req.body);
-      res.json(true); // KEY LINE
+    var bestMatch = 0;
+    var bestDiff = 1000; 
+
+
+    for (var i = friendsData.length - 1; i >= 0; i--) {
+
+      console.log("comparing with " + friendsData[i].name);
+
+      var totalDifference = 0;
+
+
+      for (var k = 0; k < 2; k++ ){
+        
+        //console.log("someone did " + friendsData[i].scores[k]);
+        //console.log("you entered " +  req.body.scores[k]);
+        //console.log("you differed by " + Math.abs(friendsData[i].scores[k] - req.body.scores[k]) );
+
+        totalDifference = totalDifference + Math.abs(friendsData[i].scores[k] - req.body.scores[k]);
+
+      }
+
+      if (totalDifference < bestDiff){
+        bestDiff = totalDifference;
+        bestMatch = i;
+      }
+
+      console.log("total difference for " + friendsData[i].name + " is " + totalDifference);
+
     }
+
+    console.log("=============================");
+    console.log("best person is " + friendsData[bestMatch].name + " and best score is " + bestDiff);
+    console.log("=============================");
+
+    // push in the user input into the friendArray
+    friendsData.push(req.body);
+
+    // respond back with the best match
+    res.json({name: friendsData[bestMatch].name, photo: friendsData[bestMatch].photo}); // KEY LINE
     
-    // Or false if they don't have a table
-    else{
-      waitListData.push(req.body);
-      res.json(false); // KEY LINE
-    }
-*/
-
-    //console.log(req.body);
-    friendData.push(req.body);
- 
-
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don't worry about it!
-
-  app.post('/api/clear', function(req, res){
-    // Empty out the arrays of data
-    friendData = [];
-    console.log(friendData);
-  })
 }
